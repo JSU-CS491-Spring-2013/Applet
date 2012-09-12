3220aa302032f190661a5e3941c5dc68810f4688
Tyler Pentecost <tyler8806@gmail.com>
2012-09-12 09:20:05 -0500

    I added a tab order, and I fixed some names.

--- BetterButtonPanel.java
+++ BetterButtonPanel.java
@@ -143,8 +143,8 @@ public class BetterButtonPanel extends javax.swing.JPanel {
         txtLastConjunction = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtLastText = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        cmdOkay = new javax.swing.JButton();
+        cmdCancel = new javax.swing.JButton();
         cmdTreeModel = new javax.swing.JButton();
         cmdSplit = new javax.swing.JButton();
         cmdEdit = new javax.swing.JButton();
@@ -153,12 +153,15 @@ public class BetterButtonPanel extends javax.swing.JPanel {
         comSuperSize = new javax.swing.JComboBox();
 
         txtFirstChapter.setColumns(2);
+        txtFirstChapter.setNextFocusableComponent(txtFirstVerse);
 
         jLabel1.setText(":");
 
         txtFirstVerse.setColumns(2);
+        txtFirstVerse.setNextFocusableComponent(txtFirstConjunction);
 
         txtFirstConjunction.setColumns(8);
+        txtFirstConjunction.setNextFocusableComponent(txtFirstText);
 
         txtFirstText.setColumns(17);
         txtFirstText.setLineWrap(true);
@@ -166,31 +169,43 @@ public class BetterButtonPanel extends javax.swing.JPanel {
         jScrollPane1.setViewportView(txtFirstText);
 
         txtLastChapter.setColumns(2);
+        txtLastChapter.setNextFocusableComponent(txtLastVerse);
 
         jLabel2.setText(":");
 
         txtLastVerse.setColumns(2);
+        txtLastVerse.setNextFocusableComponent(txtLastConjunction);
 
         txtLastConjunction.setColumns(8);
+        txtLastConjunction.setNextFocusableComponent(txtLastText);
 
         txtLastText.setColumns(17);
         txtLastText.setLineWrap(true);
         txtLastText.setRows(5);
         jScrollPane2.setViewportView(txtLastText);
 
-        jButton1.setText("Okay");
+        cmdOkay.setText("Okay");
+        cmdOkay.setNextFocusableComponent(cmdCancel);
 
-        jButton2.setText("Cancel");
+        cmdCancel.setText("Cancel");
+        cmdCancel.setNextFocusableComponent(cmdTreeModel);
 
         cmdTreeModel.setText("Tree Model");
+        cmdTreeModel.setNextFocusableComponent(cmdSplit);
 
         cmdSplit.setText("Split");
+        cmdSplit.setNextFocusableComponent(cmdEdit);
 
         cmdEdit.setText("Edit");
+        cmdEdit.setNextFocusableComponent(cmdRemove);
 
         cmdRemove.setText("Remove");
+        cmdRemove.setNextFocusableComponent(cmdGroup);
 
         cmdGroup.setText("Group");
+        cmdGroup.setNextFocusableComponent(comSuperSize);
+
+        comSuperSize.setNextFocusableComponent(txtFirstChapter);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -222,10 +237,10 @@ public class BetterButtonPanel extends javax.swing.JPanel {
                             .addComponent(cmdGroup, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(cmdEdit, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(cmdTreeModel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+                            .addComponent(cmdOkay, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cmdCancel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(cmdSplit, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(cmdRemove, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(comSuperSize, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))))
@@ -252,8 +267,8 @@ public class BetterButtonPanel extends javax.swing.JPanel {
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(cmdOkay)
+                    .addComponent(cmdCancel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cmdTreeModel)
@@ -271,14 +286,14 @@ public class BetterButtonPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdCancel;
     private javax.swing.JButton cmdEdit;
     private javax.swing.JButton cmdGroup;
+    private javax.swing.JButton cmdOkay;
     private javax.swing.JButton cmdRemove;
     private javax.swing.JButton cmdSplit;
     private javax.swing.JButton cmdTreeModel;
     private javax.swing.JComboBox comSuperSize;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
