fb8b89af73a625e326b3a08944edf45c4e67ce6d
Tyler Pentecost <tyler8806@gmail.com>
2012-09-12 19:07:22 -0500

    More comments and love for ButtonPanel.java.

--- ButtonPanel.java
+++ ButtonPanel.java
@@ -1,18 +1,20 @@
 
 /**
- * This is the new ButtonPanel. It no longer contains two ClausePanels.
- * The UI looks the same, but it was built differently. I used the NetBeans IDE
- * GUI builder. The associated ButtonPanel.form is included.
+ * This is the new ButtonPanel. It no longer contains two ClausePanels. The UI
+ * looks the same, but it was built differently. I used the NetBeans IDE GUI
+ * builder. The associated ButtonPanel.form is included.
  */
 public class ButtonPanel extends javax.swing.JPanel {
 
     private TestApplet myParent;                // This is the instance of TestApplet that created this ButtonPanel. Once we split that huge function, we should make those new methods static.
     private XMLTreeNode firstNodeSelection;     // Holds the Node used in the firstNodeSelection box
     private XMLTreeNode lastNodeSelection;      // Holds the Node used in the lastNodeSelection box
-    
+
     /**
      * Toggle the enabled property for the buttons.
-     * @param enabled the value to assign to the enabled attribute of the buttons
+     *
+     * @param enabled the value to assign to the enabled attribute of the
+     * buttons
      */
     public final void setButtonsEnabled(boolean enabled) {
         cmdEdit.setEnabled(enabled);
@@ -21,40 +23,51 @@ public class ButtonPanel extends javax.swing.JPanel {
         cmdSplit.setEnabled(enabled);
         cmdTreeModel.setEnabled(enabled);
     }
-    
+
     /**
      * Creates new form ButtonPanel
      */
     public ButtonPanel(TestApplet myParent) {
         initComponents();
-        
+
         Clause temp = null;
         setButtonsEnabled(false);
         this.myParent = myParent;
     }
 
     /**
-     * Sets the Combo Box with the correct number of Nodes to be grouped?
-     * That's what their comment said. In reality, it lists 1 through n where
-     * n is the total number of nodes minus the index of the node you select.
-     * The lower down the tree you select a node, the smaller n will be.
+     * Sets the Combo Box with the correct number of Nodes to be grouped? That's
+     * what their comment said. In reality, it lists 1 through n where n is the
+     * total number of nodes minus the index of the node you select. The lower
+     * down the tree you select a node, the smaller n will be.
      */
     public void populateComboBox() {
         comComboBox.removeAllItems(); // I added this. The previous would just keep adding them for forever.
-        XMLTreeNode parent = (XMLTreeNode) myParent.getNodePanel().getSelected().getParent(); // Gets the parent. You read from left to right just like normal. Nodes to the right are child nodes.
-        int currentI = parent.getIndex(myParent.getNodePanel().getSelected()) + 1;
-        int totalC = parent.getChildCount();
-        for (int i = 0; i < totalC - currentI; i++) {
-            comComboBox.addItem(new Integer(i + 1));
+
+        try {
+            XMLTreeNode parent = (XMLTreeNode) myParent.getNodePanel().getSelected().getParent(); // Gets the parent. You read from left to right just like normal. Nodes to the right are child nodes.
+            int currentI = parent.getIndex(myParent.getNodePanel().getSelected()) + 1;
+            int totalC = parent.getChildCount();
+            for (int i = 0; i < totalC - currentI; i++) {
+                comComboBox.addItem(new Integer(i + 1));
+            }
+        } catch (java.lang.NullPointerException npe) {
         }
     }
-    
+
+    /**
+     * Logically sets the first Node selected, and displays its information in
+     * the top box.
+     *
+     * @param selected the Node to be selected
+     */
     public void setFirstNodeSelection(XMLTreeNode selected) {
         // Try to set the information based on the Node.
         try {
             setButtonsEnabled(true);
-            
             firstNodeSelection = selected;
+            
+            // Populate the top box with the Node's information.
             txtFirstChapter.setText(selected.getChap());
             txtFirstVerse.setText(selected.getVrse());
             txtFirstConjunction.setText(selected.getConj());
@@ -64,18 +77,26 @@ public class ButtonPanel extends javax.swing.JPanel {
             txtFirstVerse.setText("");
             txtFirstConjunction.setText("");
             txtFirstText.setText("");
-            
             txtLastChapter.setText("");
             txtLastVerse.setText("");
             txtLastConjunction.setText("");
             txtLastText.setText("");
         }
     }
-    
+
+    /**
+     * Logically sets the last Node selected, and displays its information in
+     * the bottom box.
+     *
+     * @param selected the Node to be selected
+     */
     public void setLastNodeSelection(XMLTreeNode selected) {
         // Try to set the information based on the Node.
         try {
             setButtonsEnabled(true); // Note that the buttons will not be turned on if this section fails.
+            lastNodeSelection = selected;
+            
+            // Populate the botton box with the Node's information.
             txtLastChapter.setText(selected.getChap());
             txtLastVerse.setText(selected.getVrse());
             txtLastConjunction.setText(selected.getConj());
@@ -85,32 +106,56 @@ public class ButtonPanel extends javax.swing.JPanel {
             txtFirstVerse.setText("");
             txtFirstConjunction.setText("");
             txtFirstText.setText("");
+            txtLastChapter.setText("");
+            txtLastVerse.setText("");
+            txtLastConjunction.setText("");
+            txtLastText.setText("");
         }
     }
-    
+
+    /**
+     * Returns the Node represented in the top box.
+     *
+     * @return the Node in the top box
+     */
+    public XMLTreeNode getFirstNodeSelection() {
+        return firstNodeSelection;
+    }
+
+    /**
+     * Returns the Node represented in the bottom box.
+     *
+     * @return the Node in the bottom box
+     */
+    public XMLTreeNode getLastNodeSelection() {
+        return lastNodeSelection;
+    }
+
+    /**
+     * Returns the number associated with the selected value in comComboBox.
+     *
+     * @return an int representing the selected value
+     */
     public int getComboBoxSelectedIndex() {
         return comComboBox.getSelectedIndex();
     }
-    
-    /*public static void main(String[] args) { // Testing code.
-        try { // I want to add this code at some point. This will make it look much better, but we'll worry about making it pretty later.
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (Exception e) {
-        }
 
-        javax.swing.JFrame myFrame = new javax.swing.JFrame("Testing");
-        myFrame.setLayout(new java.awt.GridLayout(1, 1));
-        myFrame.add(new BetterButtonPanel());
-        myFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        myFrame.pack();
-        myFrame.setVisible(true);
-    }*/
-
+    /*
+     * public static void main(String[] args) { // Testing code. try { // I want
+     * to add this code at some point. This will make it look much better, but
+     * we'll worry about making it pretty later. for
+     * (javax.swing.UIManager.LookAndFeelInfo info :
+     * javax.swing.UIManager.getInstalledLookAndFeels()) { if
+     * ("Nimbus".equals(info.getName())) {
+     * javax.swing.UIManager.setLookAndFeel(info.getClassName()); break; } } }
+     * catch (Exception e) { }
+     *
+     * javax.swing.JFrame myFrame = new javax.swing.JFrame("Testing");
+     * myFrame.setLayout(new java.awt.GridLayout(1, 1)); myFrame.add(new
+     * BetterButtonPanel());
+     * myFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+     * myFrame.pack(); myFrame.setVisible(true); }
+     */
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -275,7 +320,6 @@ public class ButtonPanel extends javax.swing.JPanel {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cmdCancel;
     private javax.swing.JButton cmdEdit;
