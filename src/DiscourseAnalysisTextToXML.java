3b0497eb681aa41e3bc9de2aed5dfe9b48198fcd
James M. Jensen II <badocelot@badocelot.com>
2013-09-08 14:03:17 -0500

    Removed package declaration from DiscourseAnalysisTextToXML.java

--- src/DiscourseAnalysisTextToXML.java
+++ src/DiscourseAnalysisTextToXML.java
@@ -2,8 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs491texttoxml;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
